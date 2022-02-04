67ca26c25293ed4548de39825ed0415e94d6ad89
nickLee <ilkinm2003@gmail.com>
2022-02-04 15:13:18 +0400

    Add the ability to recieve timestamps if a string query is given or it is left blank

--- server.js
+++ server.js
@@ -9,21 +9,37 @@ app.use(cors({ optionsSuccessStatus: 200 }));
 
 app.use(express.static("public"));
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
    res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/api/hello", function (req, res) {
+app.get("/api/hello", (req, res) => {
    res.json({ greeting: "hello API" });
+});
+
+app.get("/api/timestamp", (req, res) => {
+  let now = new Date();
+
+  res.json({
+    "unix": now.getTime(),
+    "utc": now.toUTCString()
+  });
 });
 
 app.get("/api/timestamp/:date_string", (req, res) => {
    let dateString = req.params.date_string;
-   console.log(dateString);
+   let passedInValue = new Date(dateString);
 
-   res.json({ "error": "Invalid Date" });
+   if (passedInValue == "Invalid Date") {
+      res.json({ error: "Invalid Date" });
+   } else {
+      res.json({
+         unix: passedInValue.getTime(),
+         utc: passedInValue.toUTCString(),
+      });
+   }
 });
 
-var listener = app.listen(port, function () {
+var listener = app.listen(port, () => {
    console.log("Your app is listening on port " + listener.address().port);
 });
