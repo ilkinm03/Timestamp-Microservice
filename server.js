5ba0873e12415ef52f0e840b1871cd3c845710cd
nickLee <ilkinm2003@gmail.com>
2022-02-04 14:38:45 +0400

    Add invalid date API endpoint

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(cors({optionsSuccessStatus: 200}));
+app.use(cors({ optionsSuccessStatus: 200 }));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+   res.sendFile(__dirname + "/views/index.html");
 });
 
-
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+   res.json({ greeting: "hello API" });
+});
+
+app.get("/api/timestamp/:date_string", (req, res) => {
+   let dateString = req.params.date_string;
+   console.log(dateString);
+   
+   res.json({ error: "Invalid Date" });
 });
 
 var listener = app.listen(port, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+   console.log("Your app is listening on port " + listener.address().port);
 });
